f6a7761d8e61809db6d221a27bc155bc2ef07645
nobita2906 <140187170+nobita2906@users.noreply.github.com>
2024-09-16 08:37:03 +0700

    Update hoi.bat

--- hoi.bat
+++ hoi.bat
@@ -1,17 +1,13 @@
 @echo off
 chcp 65001 >nul
 
-:ask_webrtc
-echo Da chay Start WebRTC chua? 
-echo Chua chay thi chay di roi chon Yes...
-
-:: Sử dụng PowerShell để hiện lên hộp thoại với thông báo trên nhiều dòng
-powershell -command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $message = 'Đã chạy Start WebRTC chưa?' + [Environment]::NewLine + 'Chạy rồi thì chọn Yes đi' + [Environment]::NewLine + 'Chưa chạy thì chạy đi rồi chọn Yes'; $result = [System.Windows.Forms.MessageBox]::Show($message, 'Thông báo', [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question); exit ([int]$result)"
+:: Ẩn CMD và chạy PowerShell để hiển thị thông báo
+powershell -WindowStyle Hidden -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $message = 'Đã chạy Start WebRTC chưa?' + [Environment]::NewLine + 'Chạy rồi thì chọn Yes đi' + [Environment]::NewLine + 'Chưa chạy thì chạy đi rồi chọn Yes'; $result = [System.Windows.Forms.MessageBox]::Show($message, 'Thông báo', [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question); exit ([int]$result)"
 
 :: Kiểm tra kết quả trả về từ hộp thoại PowerShell
 if %errorlevel%==6 (
     echo Ban da chay WebRTC, mo Chrome...
-    start chrome
+    
     exit
 ) else (
     echo Ban chua chay Start WebRTC. -> Chay Start WebRTC di roi hay mo Chrome
