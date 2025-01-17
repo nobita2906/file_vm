880d0fc729a9a329c2c3be0745a620e7a3743ca0
nobita2906 <140187170+nobita2906@users.noreply.github.com>
2025-01-17 10:44:48 +0700

    Update installRustdesk.bat

--- installRustdesk.bat
+++ installRustdesk.bat
@@ -41,19 +41,3 @@ echo RustDesk ID: %rustdesk_id%
 REM Show the value of the Password Variable
 echo Password: %rustdesk_pw%
 echo ...............................................
-
-:: Yêu cầu người dùng nhập tên mới cho máy tính
-set /p UserName=Nhap ten moi cho may tinh (theo ten may duoc cap, vi du: G2xxx): 
-
-:: Tạo tên mới cho máy tính
-set NewName=PC-%UserName%
-
-:: Đổi tên máy tính
-wmic computersystem where name="%computername%" call rename name="%NewName%"
-
-:: Thông báo cho người dùng rằng máy tính sẽ được đổi tên
-echo May tinh se duoc doi ten thanh %NewName%
-
-:: Kết thúc
-endlocal
-pause
